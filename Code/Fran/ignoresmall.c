9160e32385ff8d9ab93ea5acbf3d222f0d806e1c
Raf <raf@raf-K55VD.(none)>
2013-10-06 07:15:26 +0800

    bloom 1by1

--- Code/Fran/ignoresmall.c
+++ Code/Fran/ignoresmall.c
@@ -25,75 +25,101 @@
 #include <stdlib.h>
 #include "bloom.h"
 
+#include <dirent.h>
+
 struct bloom bloom;
 
-void masterServer(FILE *f1, FILE *f2) {
+void masterServer(FILE *f1, FILE *f2, char *buffer) {
 
 	char line[300], fullline[300];
 	char *token, **ptr;
-	char *splithash[30];
+	char *splithash[8];
 	int i, j, a;
 	long num;
 	long entries = 0;
 	long hashcatalogentries = 0;
-	
+	FILE *f3;	
+	f3=fopen(buffer,"r+");
+
 	//check length of new input
 	while (fgets (line, sizeof(line), f1) != NULL) {
 		entries++;
 	}
-	
+
 	//check length of hashcatalog
 	while (fgets (line, sizeof(line), f2) != NULL) {
 		hashcatalogentries++;
 	}
-	
+
 	rewind(f1);
 	rewind(f2);
 	bloom_init(&bloom, hashcatalogentries + entries, 0.01);
-	
+
 	//add hashcatalog entries to bloom filter
 	while (fgets (line, sizeof(line), f2) != NULL) {
-	
+
 		i = 0;
 		splithash[i] = strtok(line, "|");
 		while (splithash[i] != NULL)
 			splithash[++i] = strtok(NULL, "|");
-		
+
 		bloom_add(&bloom, splithash[1], strlen(splithash[1]));
 	}
-	
-	
+
+
 	//read list of updated files
 	while (fgets (line, sizeof(line), f1) != NULL) {
-		
+
 		i = 0;
-		fullline = line;
+		//fullline = line;
 		strcpy(fullline, line);
 		splithash[i] = strtok(line, "|");
 		while (splithash[i] != NULL)
 			splithash[++i] = strtok(NULL, "|");
-		
+
 		//Size Filter
 		num = atoi(splithash[2]);
 		if (num < 8000) {
 			continue;
 		}
-	
+
 		//Bloom Filter		
 		if (bloom_add(&bloom, splithash[1], strlen(splithash[1])) == 0) {
 			fprintf(f2, "%s", fullline);
+			fprintf(f3, "%s", fullline);
 		}
 	}
-	
+	fclose(f3);
+
 }
 
 main () {
 
 	FILE *f1, *f2;
-	f1 = fopen ("out.txt", "r");
-	f2 = fopen ("hashcatalog.txt", "r+");
-	
-	//Function to read file with hash functions and ignore small files
-	masterServer(f1, f2);
-	
+	f2=fopen("hashcatalog.txt", "r+");
+
+
+	DIR *dir;
+    struct dirent *ent;
+	if (!(dir = opendir("./Outputs"))) return;
+    if (!(ent = readdir(dir))) return;
+    
+    do{
+
+    	
+    	if(strcmp(ent->d_name,"..")!=0 && strcmp(ent->d_name,".")!=0){
+    		char buffer[strlen(ent->d_name)+10];
+    		strcpy(buffer,"./Outputs/");
+    		strcat(buffer,ent->d_name);
+
+			if(strstr(buffer,".ret")!=NULL) continue;
+
+    		f1 = fopen (buffer, "r");
+    		strcat(buffer,".ret");
+    		
+    		//Function to read file with hash functions and ignore small files
+			masterServer(f1, f2,buffer);
+			fclose(f1);
+    	}
+   } while (ent = readdir(dir));
 }
