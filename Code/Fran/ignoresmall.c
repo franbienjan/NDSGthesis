796a3789a81e40a87c1814f699d79362bd8a5798
Raf <raf@raf-K55VD.(none)>
2013-10-07 12:20:19 +0800

    push

--- Code/Fran/ignoresmall.c
+++ Code/Fran/ignoresmall.c
@@ -1,4 +1,4 @@
-
+/*
 
 //**********************************************************************
 //* TASKS TO COMPLETE:
@@ -130,4 +130,147 @@ main () {
 			fclose(f1);
     	}
    } while (ent = readdir(dir));
+}
+*/
+
+//IGNORE SMALL FILES
+
+//**********************************************************************
+//* TASKS TO COMPLETE:
+//* [X] Fix Bloom Filter Data Structure
+//* [X] Compare and filter out duplicate files
+//* [X] Ignore and filter out small files (<8000)
+//* [ ] Read paper on SIFL and File Locality
+//*     >> [ ] Compare file to folder (in extreme binning-ish technique)
+//*     >> [ ] Stream Informed File Layout technique
+//*		>> [ ] Cache files
+//**********************************************************************
+
+//**********************************************************************
+//* NOTES:
+//*	[X] Hash lang ang ilagay Bloom Filter, pero store yung whole
+//* [ ] Produce a return text file for LCD Stage of the game
+//* [ ] 
+//*
+//**********************************************************************
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include "bloom.h"
+
+#include <dirent.h>
+
+struct bloom bloom;
+
+void masterServer(FILE *f1, FILE *f2) {
+
+	char line[300], fullline[300];
+	char *token, **ptr;
+	char *splithash[30];
+	int i, j, a;
+	long num;
+	long entries = 0;
+	long hashcatalogentries = 0;
+	
+	for(i=0;i<30;i++) splithash[i]=NULL;
+
+	//check length of new input
+	while (fgets (line, sizeof(line), f1) != NULL) {
+		entries++;
+	}
+	
+	//check length of hashcatalog
+	while (fgets (line, sizeof(line), f2) != NULL) {
+		hashcatalogentries++;
+	}
+	
+	rewind(f1);
+	rewind(f2);
+	bloom_init(&bloom, hashcatalogentries + entries, 0.01);
+	
+	//add hashcatalog entries to bloom filter
+	while (fgets (line, sizeof(line), f2) != NULL) {
+		i = 0;
+		splithash[i] = strtok(line, "|");
+		while (splithash[i] != NULL) splithash[++i] = strtok(NULL, "|");
+		
+		bloom_add(&bloom, splithash[1], strlen(splithash[1]));
+	}
+	
+	//read list of updated files
+	while (fgets (line, sizeof(line), f1) != NULL) {
+		
+		i = 0;
+		//fullline = line;
+		strcpy(fullline,line);
+		splithash[i] = strtok(line, "|");
+		
+		while (splithash[i] != NULL)
+			splithash[++i] = strtok(NULL, "|");
+		//Size Filter
+		num = atoi(splithash[2]);
+		if (num < 8000) {
+			continue;
+		}
+		
+		//Bloom Filter		
+		if (bloom_add(&bloom, splithash[1], strlen(splithash[1])) == 0) {
+			fprintf(f2, "%s", fullline);
+			//printf("\n%s",fullline);
+		}
+	}
+	
+}
+
+main () {
+
+	FILE *f1, *f2;
+	//f1 = fopen ("./Outputs/out1.txt", "r");
+	f2 = fopen ("hashcatalog.txt", "r+");
+	
+	//Function to read file with hash functions and ignore small files
+	//masterServer(f1, f2);
+
+	DIR *dir;
+    struct dirent *ent;
+	if (!(dir = opendir("./Outputs"))) return;
+    if (!(ent = readdir(dir))) return;
+    /*
+    do{
+
+    	if(strcmp(ent->d_name,"..")!=0 && strcmp(ent->d_name,".")!=0){
+    		char buffer[strlen(ent->d_name)+10];
+    		strcpy(buffer,"./Outputs/");
+    		strcat(buffer,ent->d_name);
+
+			//if(strstr(buffer,".ret")!=NULL) continue;
+
+    		f1 = fopen (buffer, "r");
+    		if(!f1) printf("\nno file:%s",buffer);
+    		//strcat(buffer,".ret");
+    		
+    		//Function to read file with hash functions and ignore small files
+			//masterServer(f1, f2,buffer);
+			//masterServer(f1, f2);
+			fclose(f1);
+    	}
+   } while (ent = readdir(dir));
+	*/
+   f1=fopen("./Outputs/out1.txt","r");
+   masterServer(f1, f2);
+   fclose(f1);
+	f1=fopen("./Outputs/out2.txt","r");
+   masterServer(f1, f2);
+   fclose(f1);
+   f1=fopen("./Outputs/out3.txt","r");
+   masterServer(f1, f2);
+   fclose(f1);
+   f1=fopen("./Outputs/out4.txt","r");
+   masterServer(f1, f2);
+   fclose(f1);
+   f1=fopen("./Outputs/out5.txt","r");
+   masterServer(f1, f2);
+   fclose(f1);
 }
